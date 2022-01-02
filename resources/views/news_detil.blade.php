0a7dcaf86132cfeb081e2ba440d44b8146915d3d
unknown <mayasyafira95@gmail.com>
2022-01-02 21:01:54 +0700

    projek-c3

---
+++ resources/views/news_detil.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@section('container')
+    <div class="container mt-4">
+          <article>
+            <h2 class="mb-5">{{ $news_detil->judul_berita }}</h2>
+            <p> By. Maya Anugrah Syafira dalam kategori -> {{ $news_detil->category->nama }}</p>
+            {!! $news_detil->isi_berita !!}
+
+            <p><a href="/news">Kembali ke Berita</a></p>
+          </article>
+    </div>
+ @endsection
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\DashboardBeritaController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home' ,[
+            "title" => "Home"
+    ]);
+});
+
+Route::get('about', function () {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Maya Anugrah Syafira",
+        "email" => "mayra95@gmail.com",
+        "img" => "foto1.jpg"
+    ]);
+});
+
+Route::get('news', [BeritaController::class,'index']);
+Route::get('/news/{news_posts:slug}',[BeritaController::class, 'show']);
+
+Route::get('/login',[LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login',[LoginController::class, 'authenticate']);
+Route::post('/logout',[LoginController::class, 'logout']);
+
+Route::get('/register',[RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/dashboard',[DashboardController::class, 'index'])->middleware('auth');
+
+Route::get('/dashboard/berita/checkSlug', [DashboardBeritaController::class, 'checkSlug'])->middleware('auth');
+
+Route::resource('/dashboard/berita', DashboardBeritaController::class)->middleware('auth');
